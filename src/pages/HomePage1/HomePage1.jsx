239409959edb073b57b7278ec28fc4490186205f
Oshada Nethmina <oshadanethmina665@gmail.com>
2025-12-18 15:28:55 +0530

    add some componenets and create first two pages

--- src/pages/HomePage1/HomePage1.jsx
+++ src/pages/HomePage1/HomePage1.jsx
@@ -1,34 +1,20 @@
 import React from "react";
+import Header from "../../components/header/Header";
 import Footer from "../../components/Footer/Footer";
 import "./style.css";
 import NavbarButton from "../../components/button/NavbarButton/NavbarButton";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { Box, Typography } from "@mui/material";
-import loadImage from "../../assets/2nd Image.png";
-import Header from "../../components/header/Header";
+import loadImage from '../../assets/1st Image.png';
 import HomepageButton from "../../components/button/HomepageButton/HomepageButton";
 
-export default function HomePage2() {
+export default function HomePage1() {
     return (
         <>
             <div className="content">
-                <Header navbarButton={<NavbarButton lable="Already have a account" />} />
-                <Box
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                        paddingLeft: "100px",
-                    }}
-                >
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "center",
-                            alignItems: "start",
-                        }}
-                    >
+                <Header navbarButton={<NavbarButton label="Already have a account" />} />
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', paddingLeft: '100px' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'start' }}>
                         <h1 style={{ fontSize: "60px" }}>Life Insurance Made Simple</h1>
                         <Typography sx={{ fontSize: "20px", fontWeight: "" }}>
                             Find your goals and plan your future
@@ -37,16 +23,21 @@ export default function HomePage2() {
                             label="Get Started"
                             icon={ArrowForwardIcon}
                             iconPosition="end"
-                            sx={{ mt: "30px" }}
+                            sx={{ mt: '30px' }}
                         />
                     </div>
                     <div>
-                        <img style={{ height: "80vh" }} src={loadImage} alt="" />
+                        <img style={{ height: '80vh' }} src={loadImage} alt="" />
+
                     </div>
                 </Box>
 
                 <Footer />
             </div>
+
+
+
         </>
-    )
+
+    );
 }
